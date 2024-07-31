6b9e6108fd9241b424604df172032ad3154d0169
jb <139858294+juliabaj@users.noreply.github.com>
2024-07-31 12:38:00 +0200

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
+        //creating a vector that contains indexes of two target numbers
         vector <int> sum;
+        //vector's size
         int size = nums.size();
+        //iteration from 0 to size - 2
         for(int i = 0; i < size-1; i++)
         {
+            //iterating from number next to i to size -1
             for(int j=i+1; j < size; j++)
             {
+                #checking if sum of two numbers is equal to target, if it is then add them to vector
                 if(nums[i] + nums[j] == target)
                 {
                     sum.push_back(i);
