6c4754eed73ee9cb9a24be6171ff588a5c125b20
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-01 10:38:58 +0200

    Update AddTwoNumbers.cpp

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -5,3 +5,11 @@ public:
         ListNode *curr = dummy;
         int carry = 0;         
         int v1, v2;
+        while(l1 || l2 || carry)
+                {
+                    v1 = (l1) ? l1->val :0;
+                    v2 = (l2) ? l2->val :0;
+                int val = v1+v2+carry;
+                carry = val/10;      
+                val = val % 10;       
+                curr->next = new ListNode(val);
