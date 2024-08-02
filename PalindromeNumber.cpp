516bccfaf0e4b99af16fb7e03c32b25f886b82b3
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-02 10:49:31 +0200

    Update PalindromeNumber.cpp

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -1,11 +1,12 @@
+//Palindrome number means that it is symetrical. From backwards it is gonna come out as the same number as from the front. f.ex - 1223221
 class Solution {
 public:
-    bool isPalindrome(int x)
+    bool isPalindrome(int x)                  
     {
-        if(x < 0)
+        if(x < 0)                        //checking if the number is not negative
             return false;
 
-        if(x != 0 && x%10 == 0)
+        if(x != 0 && x%10 == 0)         //checking if there is a 0 at the end or the number is just simply 0 - then it cannot be a palindrome
             return false;
 
         int reverse=0;
