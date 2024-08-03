e0c769ca8b5935f4c73ff3d68e60834d04cbd76a
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-03 11:39:44 +0200

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -5,8 +5,7 @@ public:
         vector <int> sum;                    //creating a vector that contains indexes of two target numbers
         //vector's size
         int size = nums.size();
-        //iteration from 0 to size - 2
-        for(int i = 0; i < size-1; i++)
+        for(int i = 0; i < size-1; i++)      //iteration from 0 to size - 2
         {
             //iterating from number next to i to size -1
             for(int j=i+1; j < size; j++)
