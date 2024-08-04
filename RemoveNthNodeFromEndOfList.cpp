c3e7b1ae35b64c282d812432f9f6a3604daeb51f
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-04 21:28:39 +0200

    Update RemoveNthNodeFromEndOfList.cpp

--- RemoveNthNodeFromEndOfList.cpp
+++ RemoveNthNodeFromEndOfList.cpp
@@ -1,10 +1,12 @@
+//The data structure I used for this soultion is Linked List
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *dummy = new ListNode(0);
         dummy->next = head;
 
-        ListNode *left = dummy;   //left ustawiamy na dummy
+        ListNode *left = dummy;   // dummy
         ListNode *right= head;
 
         while(n>0 && right)
