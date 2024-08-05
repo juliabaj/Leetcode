f4ac65379717036649b872d32adb3566391f9ccf
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-05 21:24:33 +0200

    Update RemoveNthNodeFromEndOfList.cpp

--- RemoveNthNodeFromEndOfList.cpp
+++ RemoveNthNodeFromEndOfList.cpp
@@ -11,15 +11,15 @@ public:
 
         while(n>0 && right)       //While right pointer is true
         {
-            right = right -> next;   //Moving the right pointer for nth positions
-            n = n-1;
+            right = right -> next;   //Moving the right pointer for nth positions, so that it is n nodes forward the left pointer
+            n = n-1;                 // Decrementing n for 1 position
         }
         while(right)
         {
-            left = left -> next;
+            left = left -> next;    //Moving the pointers to the next nodes till the right pointer is on the last node - then left pointers points to the nth node that we want to delete
             right = right -> next;
         }
-        left->next = left -> next -> next;
-        return dummy -> next;
+        left->next = left -> next -> next;    // Deleting a node by setting up next to next after next node
+        return dummy -> next;                 // Returning a new head which left pointer points to
     }
 };
