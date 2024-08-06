484f5854cc87551b5139230e02a47dbfbc1c36e9
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-06 09:30:50 +0200

    Add files via upload

---
+++ MergeTwoSortedLists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy = new ListNode(0);
+        ListNode* head = dummy;
+        while(list1 && list2)
+        {
+            if(list1-> val < list2->val)
+            {
+                head -> next = list1;
+                list1 = list1->next;
+            }
+            else{
+            head ->next = list2;
+            list2 = list2->next; 
+            }
+            head = head ->next;
+        }
+        if(list1)
+        {
+            head -> next = list1;
+        }
+        else if(list2){
+            head -> next = list2;
+        }
+        return dummy->next;
+    }
+    
+};
