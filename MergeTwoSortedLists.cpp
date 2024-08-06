e27dd49e5da47dbdebf850875460d084b0bfc688
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-06 09:36:40 +0200

    Update MergeTwoSortedLists.cpp

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -1,13 +1,14 @@
+//In this soultion i'm using a data structure Linked List. ->val is a default variable in this structure that stores the value of the node
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *dummy = new ListNode(0);
-        ListNode* head = dummy;
-        while(list1 && list2)
+        ListNode *dummy = new ListNode(0);        //creating a dummy node in cases of errors such as deleting the first actual node
+        ListNode* head = dummy;                   // Setting dummy node to head
+        while(list1 && list2)                    //Iterating through both lists untill none of them is empty
         {
-            if(list1-> val < list2->val)
+            if(list1-> val < list2->val)         //If value of the list1's node is smaller than value of list2's node - 
             {
-                head -> next = list1;
+                head -> next = list1;            // - Then the next element is this node
                 list1 = list1->next;
             }
             else{
