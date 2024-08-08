06fdcb5751c4d155c517ed42022b09fd244a5f8c
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-08 09:38:22 +0200

    Update Sqrt(x).cpp

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        long left = 0;
+        long left = 0;               //Initializing both borders to iterate through
         long right = x;
-        int result  = 0;
+        int result  = 0;             //Variable to store result
     
-        while (left <= right){
-            long mid = left +(right-left) /2;
-            if (mid * mid > x)
+        while (left <= right){                                //Iterating untill left isn't crossing right
+            long mid = left +(right-left) /2;                 //Calculating the middle value
+            if (mid * mid > x)                                //If mid^2 is greater than x that means we have to change the right border to smaller one
             {
                 right = mid - 1;
             }
