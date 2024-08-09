713744dd3d65c454b71ad7f252a258d33f842a22
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-09 13:15:38 +0200

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* next = NULL;
+        ListNode* curr = head;            //Initializing current pointer to the head node of the list
+        ListNode* next = NULL;            //Initializing next pointer and previous pointer
         ListNode* prev = NULL;
 
         while(curr != NULL)
