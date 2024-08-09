c683ae081f4df131b31d2c37876d2279da0d7b48
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-09 13:14:13 +0200

    Add files via upload

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        ListNode* prev = NULL;
+
+        while(curr != NULL)
+        {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+         head = prev;
+         return head;
+    }
+};
