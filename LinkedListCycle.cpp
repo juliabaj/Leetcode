0a27ec433fc75298d7b0f56b777cd627730a5e17
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-13 15:26:55 +0200

    Update LinkedListCycle.cpp

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -9,7 +9,7 @@ public:
         ListNode *fast = head;          //Initializing both fast and slow pointers at the head node of the list
         ListNode *slow = head;          //Slow pointer will move one node at the time, while fast pointer will move two nodes ahead
 
-        while(fast != NULL && fast->next != NULL && slow != NULL )
+        while(fast != NULL && fast->next != NULL && slow != NULL )    //Iterating through the list and checking if any of the nodes are empty
         {
             fast = fast -> next -> next;
             slow = slow -> next;
