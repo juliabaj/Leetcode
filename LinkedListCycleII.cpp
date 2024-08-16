e91e9043080f8e416ff599ee7bb4857d985cb288
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-16 10:56:42 +0200

    Update LinkedListCycleII.cpp

--- LinkedListCycleII.cpp
+++ LinkedListCycleII.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
 
-        ListNode *fast = head;
-        ListNode *slow = head;
+        ListNode *fast = head;            //Initializing fast pointer that moves 2 nodes at a time
+        ListNode *slow = head;            //Initializing slow pointer that moves 1 node at a time
         bool cycleFound = false;
 
         while( fast != NULL && fast->next != NULL && slow != NULL)
