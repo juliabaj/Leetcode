b984d54455ba01b2e0091458b6c9e8b6b73755e4
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-20 12:34:29 +0200

    Update ReverseLinkedListII.cpp

--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -10,3 +10,29 @@ public:
         ListNode* curr = head;
         ListNode* leftPrev = dummy;
         //ListNode* next = NULL;
+for(int i=1; i< left; i++)         //znalezienie left
+        {
+            leftPrev = curr;
+            curr = curr -> next;
+        }
+        // now cur = left
+        // leftprev = node before left
+
+        ListNode* prev = NULL;
+        for(int i=1; i <= right - left + 1; i++)   //reverse from l to r
+        {
+            ListNode* temp = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        // update pointers
+        leftPrev -> next -> next = curr;  //curr is node after 'right'
+        leftPrev -> next = prev;          // prev is 'right'
+        /*ListNode* NewHead = dummy -> next;
+        delete dummy;
+        return NewHead;            // dummy->next to nasze head 
+        */
+        return dummy -> next;
+    }
+};
