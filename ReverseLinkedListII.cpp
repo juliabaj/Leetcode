c475d4396ebec90a4fd590906a53c4a9324f5591
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-20 12:34:13 +0200

    Update ReverseLinkedListII.cpp

--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head || left >= right)
+        {
+            return head;
+        }
+        ListNode* dummy = new ListNode(0);
+        dummy -> next = head;
+        ListNode* curr = head;
+        ListNode* leftPrev = dummy;
+        //ListNode* next = NULL;
