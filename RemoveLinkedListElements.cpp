7b814f331debfe6c3fdabf6e3a4fe0a75d50cc30
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-23 12:14:24 +0200

    Update RemoveLinkedListElements.cpp

--- RemoveLinkedListElements.cpp
+++ RemoveLinkedListElements.cpp
@@ -1,1 +1,7 @@
-
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummy = new ListNode(0);
+        dummy -> next = head;
+        ListNode *curr = head;
+        ListNode *prev = dummy;
