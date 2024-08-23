f95db73ad7958e71ac49ae01f818cc4155325a46
jb <139858294+juliabaj@users.noreply.github.com>
2024-08-23 12:14:47 +0200

    Update RemoveLinkedListElements.cpp

--- RemoveLinkedListElements.cpp
+++ RemoveLinkedListElements.cpp
@@ -5,3 +5,19 @@ public:
         dummy -> next = head;
         ListNode *curr = head;
         ListNode *prev = dummy;
+        while(curr)
+                {
+                    ListNode *nxt = curr -> next;
+        
+                    if(curr->val == val)
+                    {
+                        prev -> next = nxt;  
+                    }
+                    else 
+                        prev = curr;
+                        curr = nxt;
+                }
+                return dummy -> next;
+                
+            }
+        };
