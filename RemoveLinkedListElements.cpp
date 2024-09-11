2653c1148bfdef2308555381d3b5d256c0ae7769
jb <139858294+juliabaj@users.noreply.github.com>
2024-09-11 12:28:34 +0200

    Update RemoveLinkedListElements.cpp

--- RemoveLinkedListElements.cpp
+++ RemoveLinkedListElements.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode *dummy = new ListNode(0);
-        dummy -> next = head;
-        ListNode *curr = head;
-        ListNode *prev = dummy;
+        ListNode *dummy = new ListNode(0);         //creating a dummy node as a "first" node
+        dummy -> next = head;                      //Actual first node is next to dummy
+        ListNode *curr = head;                    //Setting current pointer to head
+        ListNode *prev = dummy;                   //Setting previous pointer to dummy node
         while(curr)
                 {
                     ListNode *nxt = curr -> next;
